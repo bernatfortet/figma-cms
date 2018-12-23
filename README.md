be7ab093b1d78a80cb70f77670ed6233bc5244b2
Bernat <bernatfortet@gmail.com>
2018-12-23 10:30:25 -0500

    Add initial Test and Readme"

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# figma-cms
+# figma-csm
+Use Figma as a CMS
+
+## Installation 
+```sh
+yarn add figma-cms
+```
+## Usage
+```typescript
+// TODO
+```
+
+## Test 
+```sh
+yarn run test
+```
---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict'
+var expect = require('chai').expect
+var index = require('../dist/index.js')
+
+describe('Basic test', () => {
+
+  it('should be true', () => {
+    expect(true).to.equal(true)
+  })
+
+})
