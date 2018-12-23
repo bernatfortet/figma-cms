4ca07a98bf0a6544c377fd8d299a9047751c9689
Bernat <bernatfortet@gmail.com>
2018-12-23 11:13:14 -0500

    Use Figma client class and client Creator

--- lib/figma.ts
+++ lib/figma.ts
@@ -1,50 +1,77 @@
 import axios from 'axios'
 
-const PERSONAL_ACCESS_TOKEN = '3429-bf1d6755-7de1-446b-aace-4d1b00d1a775'
-const FILE_KEY = 'EuXW2OoSnNpWqU9ULu1sOYhN'
+type FigmaClientOptions = {
+  accessToken: string,
+  fileKey: string,
+}
 
-function fetchFigma( endpoint: string ){
-  const options = {
-    method: 'GET',
-    headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN },
-    url: `https://api.figma.com/v1/${endpoint}`,
+export function createClient(options: FigmaClientOptions) {
+  return new FigmaClient(options)
+}
+
+class FigmaClient {
+  accessToken: string = ''
+  fileKey: string = ''
+
+  file: any // TODO
+
+  constructor({ accessToken, fileKey }: FigmaClientOptions ){
+    this.accessToken = accessToken
+    this.fileKey = fileKey
+    this.file = this.fetchFile(fileKey)
   }
 
-  return axios(options)
-    .then( response => {
-      return response.data
-    })
-    .catch( error => ({ err: error }))
+  async fetchFile(fileKey: string) {
+    const file = await this.fetch(`files/${fileKey}`)
+    if (!file || !file.document) throw ('File not found')
+    return file.document
+  }
+
+  getFigmaCanvas(file, canvasName: string) {
+    const canvas = file.children.filter(el => el.type == 'CANVAS' && el.name == canvasName)[0]
+    return canvas
+  }
+
+  getFrameFromCanvas(canvas, frameName: string) {
+    const frame = canvas.children.filter(el => el.type == 'FRAME' && el.name == frameName)[0]
+    return frame
+  }
+
+  fetch(endpoint: string) {
+    const options = {
+      method: 'GET',
+      url: `https://api.figma.com/v1/${endpoint}`,
+      headers: { "x-figma-token": this.accessToken },
+    }
+
+    return axios(options)
+      .then(response => {
+        return response.data
+      })
+      .catch( error => ({ err: error }))
+  }
+
+
+  // async function fetchImages(imageIds) {
+  //   const parsedImageIds = imageIds.join(',')
+  //   const result = await fetchFigma(`images/${FILE_KEY}?ids=${parsedImageIds}`)
+  //   return result.images
+  // }
+
+  // async function fetchFileImages(fileKey) {
+  //   const result = await fetchFigma(`files/${fileKey}/images`)
+  //   if (result.error) throw ('File not found')
+  //   if (!result.meta || !result.meta.images) throw ('No Images found')
+  //   return result.meta.images
+  // }
+
 }
 
-export async function fetchFile(fileKey: string ){
-  const file = await fetchFigma( `files/${fileKey}` )
-  if( !file || !file.document ) throw( 'File not found')
-  return file.document
-}
 
-export function getFigmaCanvas(file, canvasName: string ){
-  const canvas = file.children.filter( el => el.type == 'CANVAS' && el.name == canvasName)[0]
-  return canvas
-}
 
-export function getFrameFromCanvas(canvas, frameName: string ){
-  const frame = canvas.children.filter( el => el.type == 'FRAME' && el.name == frameName)[0]
-  return frame
-}
 
-export async function fetchImages( imageIds ){
-  const parsedImageIds = imageIds.join(',')
-  const result = await fetchFigma( `images/${FILE_KEY}?ids=${parsedImageIds}` )
-  return result.images
-}
 
-export async function fetchFileImages( fileKey ){
-  const result = await fetchFigma( `files/${fileKey}/images` )
-  if( result.error ) throw( 'File not found')
-  if( !result.meta || !result.meta.images ) throw( 'No Images found')
-  return result.meta.images
-}
+
 
 
 export function parseValueProps( website ){
